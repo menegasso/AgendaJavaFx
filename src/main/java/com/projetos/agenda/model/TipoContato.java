d9e506eac8a739f1c97682aa8f2a076011ba30ba
andre.menegasso <andre.menegasso@pti.org.br>
2022-09-21 01:40:04 -0300

    Alterações das anotações para as classes modelo

--- src/main/java/com/projetos/agenda/model/TipoContato.java
+++ src/main/java/com/projetos/agenda/model/TipoContato.java
@@ -1,15 +1,24 @@
 package com.projetos.agenda.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
 public class TipoContato {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "descricao", nullable = false)
     private String descricao;
-    
-    
-    
+
 }
