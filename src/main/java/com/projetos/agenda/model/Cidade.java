f39aaa77e912c7b5d193617912ba124cc7c103ee
andre.menegasso <andre.menegasso@pti.org.br>
2022-09-21 01:40:04 -0300

    Alterações das anotações para as classes modelo

--- src/main/java/com/projetos/agenda/model/Cidade.java
+++ src/main/java/com/projetos/agenda/model/Cidade.java
@@ -1,15 +1,30 @@
 package com.projetos.agenda.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Cidade {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "descricao", nullable = false)
     private String descricao;
+
+    @Column(name = "uf", nullable = false)
     private String uf;
+
+    @Column(name = "cep", nullable = false)
     private String cep;
 
 }
--- src/main/java/com/projetos/agenda/model/Contato.java
+++ src/main/java/com/projetos/agenda/model/Contato.java
@@ -1,21 +1,49 @@
 package com.projetos.agenda.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Contato {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "endereco")
     private String endereco;
-    private int numero;
-    private int idCidade;
+
+    @Column(name = "numero")
+    private Long numero;
+
+    @OneToOne
+    private Long idCidade;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "nascimento")
     private String nascimento;
+
+    @Column(name = "telefone1")
     private String telefone1;
+
+    @Column(name = "telefone2")
     private String telefone2;
-    private int idTipoContato;
+
+    @OneToOne
+    private Long idTipoContato;
 
 }
